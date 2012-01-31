84c091a9bb8351a0030308925c08ad8903cb770e
slavestys <slavestys@gmail.com>
2012-01-31 22:11:02 +0400

    Done with user edit/update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,6 +16,17 @@ module SessionsHelper
    def current_user=(user)
     @current_user = user
   end
+  def deny_access
+   store_location
+    redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
+  def current_user?(user)
+    user == current_user
+  end
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
   private
 
     def user_from_remember_token
@@ -24,6 +35,13 @@ module SessionsHelper
 
     def remember_token
       cookies.signed[:remember_token] || nil
-    end    
+    end   
+    def store_location
+      session[:return_to] = request.fullpath
+    end
+
+    def clear_return_to
+      session[:return_to] = nil
+    end 
 end
 
